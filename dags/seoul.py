2c0e23572dca189bd7b42bf851213f32b196b0d7
jacob-53 <jacob8753@gmail.com>
2025-03-11 18:08:52 +0900

    batch alert

--- dags/seoul.py
+++ dags/seoul.py
@@ -20,13 +20,14 @@ with DAG(
         task_id="b_1",
         bash_command="""
         echo "date ======================> 'date'",
-        echo "data_interval_start =======> {{data_interval_start}}",
-        echo "data_interval_end =========> {{data_interval_end}}",
-        echo "ds=========================>{{ds}}",
+        echo "data_interval_start =======> {{data_interval_start.in_tz("Asia/Seoul")}}",
+        echo "data_interval_end =========> {{data_interval_end.in_tz("Asia/Seoul")}}",
+        echo "ds=========================>{{ds.replace("-", "/")}}",
         echo "ts=========================>{{ts}}",
         echo "execution_date ============>{{execution_date}}",
         echo "prev_execution_date =======>{{prev_execution_date}}",
-        echo "next_execution_date =======>{{next_execution_date}}"      
+        echo "next_execution_date =======>{{next_execution_date}}",
+        echo "test=======================>{{data_interval_start.in_tz('Asia/Seoul').strftime('%Y/%m/%d/%H')}}"      
         """)
     b2_1=BashOperator(task_id="b_2_1", bash_command="echo 2_1")
     b2_2=BashOperator(task_id="b_2_2", bash_command="echo 2_2")
@@ -49,4 +50,5 @@ with DAG(
     start >> b1>> b2_1
     b1>>b2_2
     [b2_1,b2_2]>> end
+    
     
---
+++ dags/test96.py
@@ -0,0 +1,111 @@
+from datetime import datetime, timedelta
+from airflow.models.dag import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+import pendulum
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.operators.python import PythonOperator
+import requests
+import os
+from airflow.models import Variable
+
+def generate_bash_commands(columns: list):
+    cmds = []
+    max_length = max(len(c) for c in columns)
+    for c in columns:
+        # 가변적인 공백 추가 (최대 길이에 맞춰 정렬)
+        padding = " " * (max_length - len(c))
+        cmds.append(f'echo "{c}{padding} : ====> {{{{ {c} }}}}"')
+    return "\n".join(cmds)
+
+local_tz = pendulum.timezone("Asia/Seoul")
+
+def print_kwargs(**kwargs):
+    print("kwargs====>",kwargs)
+    for k,v in kwargs.items():
+        print(f"{k}:{v}")
+
+    WEBHOOK_ID = Variable.get('WEBHOOK_ID')
+    WEBHOOK_TOKEN = Variable.get('WEBHOOK_TOKEN')
+    WEBHOOK_URL = f"https://discordapp.com/api/webhooks/{WEBHOOK_ID}/{WEBHOOK_TOKEN}"
+    data = {"content": f"Test {kwargs['dag'].dag_display_name} {kwargs['task'].task_id} OK Jacob" }
+    response = requests.post(WEBHOOK_URL, json=data)
+
+    if response.status_code == 204:
+        print("메시지가 성공적으로 전송되었습니다.")
+    else:
+        print(f"에러 발생: {response.status_code}, {response.text}")
+        
+# Directed Acyclic Graph
+with DAG(
+    "seoul_test",
+    schedule="@hourly",
+    # start_date=datetime(2025, 3, 10)
+    start_date=pendulum.datetime(2025, 3, 11, tz="Asia/Seoul")
+) as dag:
+    
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
+    send_notification = PythonOperator(
+        task_id="sensend_notification",
+        python_callable=print_kwargs
+    )
+    columns_b1 = [
+    "data_interval_start", "data_interval_end", "logical_date", "ds", "ds_nodash",
+    # "exception",
+    "ts", "ts_nodash_with_tz", "ts_nodash", "prev_data_interval_start_success",
+    "prev_data_interval_end_success", "prev_start_date_success", "prev_end_date_success",
+    "inlets", "inlet_events", "outlets", "outlet_events", "dag", "task", "macros",
+    "task_instance", "ti", "params", "var.value", "var.json", "conn", "task_instance_key_str",
+    "run_id", "dag_run", "map_index_template", "expanded_ti_count", "triggering_dataset_events"
+    ]
+    cmds_b1 = generate_bash_commands(columns_b1)
+   
+    b1 = BashOperator(
+        task_id="b_1", 
+        bash_command=f"""
+            echo "date ====================> `date`"
+            {cmds_b1}
+        """)
+    
+    cmds_b2_1 = [
+    "execution_date",
+    "next_execution_date","next_ds","next_ds_nodash",
+    "prev_execution_date","prev_ds","prev_ds_nodash",
+    "yesterday_ds","yesterday_ds_nodash",
+    "tomorrow_ds", "tomorrow_ds_nodash",
+    "prev_execution_date_success",
+    "conf"
+    ]
+    
+    cmds_b2_1 = generate_bash_commands(cmds_b2_1)
+   
+    b2_1 = BashOperator(task_id="b_2_1", 
+                        bash_command=f"""
+                        {cmds_b2_1}
+                        """)
+    
+    b2_2 = BashOperator(task_id="b_2_2", 
+                        bash_command="""
+                        echo "data_interval_start : {{ data_interval_start.in_tz('Asia/Seoul') }}"
+                        """)
+    mkdir = BashOperator(task_id="mkdir",
+                         bash_command="""
+                         echo "data_interval_start : {{data_interval_start.in_tz('Asia/Seoul').strftime('%Y%m%d%H')}}",
+                         mkdir -p ~/data/seoul/{{data_interval_start.in_tz('Asia/Seoul').strftime('%Y/%m/%d/%H')}}
+                         """)
+    #send_notification = BashOperator(task_id="send_notification",
+    #                                 bash_command="bash -c 'source ~/.zshrc && ~/data/mkdiralert.sh'",
+    #                                 trigger_rule=TriggerRule.ONE_FAILED 
+    #                                )
+    start >> b1 >> [b2_1, b2_2] >> mkdir >> [end,send_notification]
+    
+    # start >> b1
+    # b1 >> [b2_1, b2_2]
+    # [b2_1, b2_2] >> end
+    
+    # start >> b1 >> b2_1
+    # b1 >> b2_2
+    # [b2_1, b2_2] >> end
+if __name__ == "__main__":
+    dag.test()
