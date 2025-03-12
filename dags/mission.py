4c3d19337568b37308a87b52dd18fc0eb120e95c
jacob-53 <jacob8753@gmail.com>
2025-03-12 22:27:26 +0900

    mission

--- dags/mission.py
+++ dags/mission.py
@@ -6,23 +6,29 @@ import pendulum
 from airflow.utils.trigger_rule import TriggerRule
 from airflow.operators.python import PythonOperator
 from airflow.operators.python import PythonVirtualenvOperator
+
+
 def abc():
     pass
+
 with DAG(
-    "mission",
+    "myetl",
     #schedule=timedelta(days=1),
     #schedule="* * * * * *",
     schedule="@hourly",
-    start_date=pendulum.datetime(2025,3,12, tz="Asia/Seoul")
+    start_date=pendulum.datetime(2025,3,12, tz="Asia/Seoul"),
+    catchup=True,
     ) as dag:
     
     start = EmptyOperator(task_id="start")
     end = EmptyOperator(task_id="end")
     
-    make_data = BashOperator( task_id="mk_data",
-        bash_command="""
-        
-        """)
+    make_data = BashOperator(
+                task_id="mk_data",
+                bash_command=
+                "bash /home/jacob/airflow/make_data.sh /home/jacob/data/{{data_interval_start.in_tz('Asia/Seoul').strftime('%Y/%m/%d/%H')}}"
+                )
+    
     load_data = PythonVirtualenvOperator(task_id="load_data",
                                             python_callable= abc,
                                             requirements=["git+https://github.com/Jacob-53/myairflow.git@0.1.0"],)
